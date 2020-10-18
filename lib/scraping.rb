309c82f5490059e7c2bbba7797504b9c16fb1cc4
ichihara-development-github <haken.ichihara@icloud.com>
2020-10-18 22:56:44 +0900

    test

--- lib/scraping.rb
+++ lib/scraping.rb
@@ -4,37 +4,38 @@ module Scraping
   require 'open-uri'
   require "csv"
   require "byebug"
-  require "selenium-webdriver"
+  require "selenium-web$driver"
 
   RAKUTEN_URL = "https://www.rakuten.co.jp/"
   AMAZON_URL = "https://www.amazon.co.jp/"
+
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.binary = ENV["CHROME_SHIM"]
+  options.add_argument('--disable-gpu')
+  options.add_argument('--headless')
+  $driver = Selenium::WebDriver.for :chrome, options: options
 
   def collect_rakuten(search_elm)
     p "hello"
   end
 
   def collect_amazon(search_elm)
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.binary = ENV["CHROME_SHIM"]
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-    driver = Selenium::WebDriver.for :chrome, options: options
-    driver.get(AMAZON_URL)
-    search_box = driver.find_element(:id, 'twotabsearchtextbox')
-    btn = driver.find_element(:xpath, '//*[@id="nav-search"]/form/div[3]/div')
+    $driver.get(AMAZON_URL)
+    search_box = $driver.find_element(:id, 'twotabsearchtextbox')
+    btn = $driver.find_element(:xpath, '//*[@id="nav-search"]/form/div[3]/div')
     search_box.send_keys(search_elm)
     btn.click
     sleep 3
-    select = Selenium::WebDriver::Support::Select.new(driver.find_element(:id, "s-result-sort-select"))
+    select = Selenium::WebDriver::Support::Select.new($driver.find_element(:id, "s-result-sort-select"))
     select.select_by(:value, "review-rank")
     sleep 3
-    amazon = scrap_price("a-price-whole",driver)
-    driver.close
+    amazon = scrap_price("a-price-whole")
+    $driver.close
     amazon
   end
 
-  def scrap_price(elm, driver)
-    prices = driver.find_elements(:class, elm)
+  def scrap_price(elm)
+    prices = $driver.find_elements(:class, elm)
     price_list = prices.map{|price| price.text.delete!("￥,").to_i}
   end
 
