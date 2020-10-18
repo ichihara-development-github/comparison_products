907462f947b06b12b69a1e11d105f85e33403c7d
ichihara-development-github <haken.ichihara@icloud.com>
2020-10-19 02:04:22 +0900

    test

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -12,6 +12,7 @@
   def new_browser
     options = Selenium::WebDriver::Chrome::Options.new
     options.binary = ENV['GOOGLE_CHROME_SHIM']
+    options.add_argument('--no-sandbox')  
     options.add_argument('--disable-gpu')
     options.add_argument('--headless')
     options.add_argument('--remote-debugging-port=9222')
