460f94cf38e4dfd95349800b0e5084939624d655
ichihara-development-github <haken.ichihara@icloud.com>
2020-10-18 22:59:49 +0900

    test

--- lib/scraping.rb
+++ lib/scraping.rb
@@ -4,7 +4,7 @@ module Scraping
   require 'open-uri'
   require "csv"
   require "byebug"
-  require "selenium-web$driver"
+  require "selenium-webDRIVER"
 
   RAKUTEN_URL = "https://www.rakuten.co.jp/"
   AMAZON_URL = "https://www.amazon.co.jp/"
@@ -13,29 +13,29 @@ module Scraping
   options.binary = ENV["CHROME_SHIM"]
   options.add_argument('--disable-gpu')
   options.add_argument('--headless')
-  $driver = Selenium::WebDriver.for :chrome, options: options
+  DRIVER = Selenium::WebDriver.for :chrome, options: options
 
   def collect_rakuten(search_elm)
     p "hello"
   end
 
   def collect_amazon(search_elm)
-    $driver.get(AMAZON_URL)
-    search_box = $driver.find_element(:id, 'twotabsearchtextbox')
-    btn = $driver.find_element(:xpath, '//*[@id="nav-search"]/form/div[3]/div')
+    DRIVER.get(AMAZON_URL)
+    search_box = DRIVER.find_element(:id, 'twotabsearchtextbox')
+    btn = DRIVER.find_element(:xpath, '//*[@id="nav-search"]/form/div[3]/div')
     search_box.send_keys(search_elm)
     btn.click
     sleep 3
-    select = Selenium::WebDriver::Support::Select.new($driver.find_element(:id, "s-result-sort-select"))
+    select = Selenium::WebDriver::Support::Select.new(DRIVER.find_element(:id, "s-result-sort-select"))
     select.select_by(:value, "review-rank")
     sleep 3
     amazon = scrap_price("a-price-whole")
-    $driver.close
+    DRIVER.close
     amazon
   end
 
   def scrap_price(elm)
-    prices = $driver.find_elements(:class, elm)
+    prices = DRIVER.find_elements(:class, elm)
     price_list = prices.map{|price| price.text.delete!("￥,").to_i}
   end
 
